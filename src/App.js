d0bf52d3da15f8fb78aa74c805e89d5d2630f943
Nikola Cerovina <cerovina1997@gmail.com>
2023-06-23 23:52:55 +0200

    Added changes

--- src/App.js
+++ src/App.js
@@ -14,39 +14,53 @@ const WeatherApp = () => {
         `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}`
       );
       const data = await response.json();
-      setWeather(data);
+
+      if (response.ok) {
+        setWeather(data);
+      } else {
+        throw new Error(data.message);
+      }
     } catch (error) {
-      console.error('Error fetching weather data:', error);
+      alert('You are making Nikola upset with your laziness!');
+      console.error('No city name', error);
     }
   };
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    fetchWeatherData();
+    if (city.trim() !== '') {
+      fetchWeatherData();
+    } else {
+      alert('Please enter a city name.');
+    }
   };
 
   return (
-    <><div className='yetipic'>
-          <img src={myImage} alt="Yeti" />
-      </div><div className="weather-container">
-              <h1>Weather Yeti</h1>
-              <form onSubmit={handleSubmit}>
-                  <input
-                      type="text"
-                      placeholder="Enter city name"
-                      value={city}
-                      onChange={(e) => setCity(e.target.value)} />
-                  <button type="submit">Get Weather</button>
-              </form>
+    <>
+      <div className='yetipic'>
+        <img src={myImage} alt="Yeti" />
+      </div>
+      <div className="weather-container">
+        <h1>Weather Yeti</h1>
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            placeholder="Enter city name"
+            value={city}
+            onChange={(e) => setCity(e.target.value)}
+          />
+          <button type="submit">Get Weather</button>
+        </form>
 
-              {weather && (
-                  <div className='info'>
-                      <h2>{weather.name}</h2>
-                      <p>Temperature: {Math.round(weather.main.temp - 273.15)}°C</p>
-                      <p>Weather: {weather.weather[0].main}</p>
-                  </div>
-              )}
-          </div></>
+        {weather && (
+          <div className='info'>
+            <h2>{weather.name}</h2>
+            <p>Temperature: {Math.round(weather.main.temp - 273.15)}°C</p>
+            <p>Weather: {weather.weather[0].main}</p>
+          </div>
+        )}
+      </div>
+    </>
   );
 };
 
